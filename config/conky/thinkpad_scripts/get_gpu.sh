67fc8c3ab56cf3b5d31dff6d52566b19f7a95813
Eglantine Kremer-Cochet <eglantine.kremer-cochet@etu.univ-lyon1.fr>
2022-10-30 08:04:46 +0100

    add conky thinkpad

---
+++ config/conky/thinkpad_scripts/get_gpu.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+#
+# Author: u0xpsec     2018
+#
+if [ $1 = "gpu" ];then
+   neofetch | grep "GPU" | cut -d" " -f2- | sed 's/\x1B\[[0-9;]*m//g' | sed 's\ \\4'
+elif [ $1 = "res" ];then
+   neofetch | grep "Resolution" | cut -d" " -f2 | sed 's/\x1B\[[0-9;]*m//g'
+else
+   echo "Unkown"
+fi
